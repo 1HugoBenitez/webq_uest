54fce326a1148e3f355cc871f986bcb2b8a22d0f
Hugo Medrano <benitezmedrano_34@hotmail.com>
2019-04-14 16:46:49 -0600

    no message
    Signed-off-by: Hugo Medrano <benitezmedrano_34@hotmail.com>

---
+++ git.kt
@@ -0,0 +1,51 @@
+/*¿Que es un controlador de versiones?
+    El control de versiones es un sistema que registra los cambios realizados sobre un archivo o conjunto de archivos 
+    a lo largo del tiempo, de modo que puedas recuperar versiones específicas más adelante.
+    A pesar de que los ejemplos de este libro muestran código fuente como archivos bajo control de versiones,
+    en realidad cualquier tipo de archivo que encuentres en un ordenador puede ponerse bajo control de versiones.
+
+¿Cuales son las ventajas de utlizar un controlador de versiones?
+    Muy versátil y con figurable.
+    Distribuido, podemos hace commits en nuestra maquina local, por lo que podemos seguir trabajando sin conexión
+    a Internet subir los cambios mas tarde.
+    Rápido, la mayoría de operaciones son locales,
+    resolución de conflictos a travez de herramientas  mergetool y el editor meld.
+    Seguro, se puede trabajar incluso si el servidor esta caído sin perder tiempo de trabajo.
+    La posibilidad de ver  rápidamente la historia del proyecto con el comando git log.
+
+¿Cuales son los controladores de versiones disponibles para android studio?
+    -GitLab
+    -GitHub
+    -BitBucket
+    -Azure DevOps
+
+¿Cuales son las servidores de contorles de versines mas comunes?
+    -Subversion
+    -Git 
+    -Mercurial
+    -CVS
+    -Bazaar
+Termino Push:
+    Es un comando que sube cambios hechos en tu ambito de trabajo a una rama de trabajo de mi ordenanador y/o de tu equipo remota
+
+Termino Commit:
+    Es la acción de guardar o subir tus archivos a tu repositorio remoto (es una  actualización de tus cambios) también puede hacerse 
+    al local (depende donde hayas creado tu repositorio).
+    
+Termino Branch:
+    Simplemente un apuntador móvil apuntando a una de esas confirmaciones. 
+    La rama por defecto de Git es la rama master. 
+    Con la primera confirmación de cambios que realicemos, se creará esta rama principal master apuntando a dicha confirmación.
+    En cada confirmación de cambios que realicemos, la rama irá avanzando automáticamente. Y la rama master apuntará siempre a la
+    última confirmación realizada.
+Termino Stack: 
+    Es un software que te permite configurar tu propio servidor Git privado para Windows. 
+    Esto significa que usted crea un sistema de versiones de vanguardia sin ningún conocimiento previo de Git. 
+    GitStack también hace que sea muy fácil proteger y mantener su servidor actualizado.
+    
+Termino Repositorio: 
+    El propósito de Git es administrar un proyecto, o un conjunto de archivos, a medida que cambian con el tiempo.
+    Git almacena esta información en una estructura de datos llamada repositorio.
+
+
+ */
